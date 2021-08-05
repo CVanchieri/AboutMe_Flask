b87269324da85be045aca645adde9220fbc4c383
Charles Vanchieri <cvanchieri6@Charless-MacBook-Pro.local>
2021-08-04 17:02:52 -0700

    updating

--- app.py
+++ app.py
@@ -14,31 +14,6 @@ app = Flask(__name__)
 # app.config['STATIC_AUTO_RELOAD'] = True
 # app.run(debug=True)
 
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    USERNAME_AUTH = os.getenv("USERNAME_AUTH")
-    PASSWORD_AUTH = os.getenv("PASSWORD_AUTH")
-    return username == USERNAME_AUTH and password == PASSWORD_AUTH
-
-def authenticate():
-    """Sends a 401 response that enables basic auth"""
-    return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
-
-
 ### home page ###
 @app.route('/')
 def root():
