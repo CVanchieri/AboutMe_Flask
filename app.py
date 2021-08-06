799d449d22276abe73c65e64e439c6c55c9254e5
Charles Vanchieri <cvanchieri6@Charless-MacBook-Pro.local>
2021-08-06 13:46:52 -0700

    trying to downsize

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 from flask import Flask, render_template, request, jsonify
 from assistant import check
 
-# FalconSQL Login https://api.plot.ly/
-
 """Create and configure an instance of the Flask application"""
 app = Flask(__name__)
 ### local development ###
@@ -12,29 +10,18 @@ app = Flask(__name__)
 # app.config['STATIC_AUTO_RELOAD'] = True
 # app.run(debug=True)
 
-### home page ###
-@app.route('/')
-def root():
-    return render_template('home.html')
-
-
 ################################
-@app.route("/chat_home", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def chat_home():
-    # if request.method == 'POST':
-    #     user_query = request.form['user_query']
-    #     print(user_query)
-    #     user_query = user_query.strip()
-    #     result = check(user_query)
-    #     return render_template('index.html', response=result, user_query=user_query)
+
     return render_template('chatbot.html')
 
 
 @app.route("/chat", methods=['GET', 'POST'])
 def chat():
     user_query = request.json
-    #print(user_query)
+
     user_query = user_query['name']
     result = check(user_query)
-    #print(result)
+
     return jsonify(result)
